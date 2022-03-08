5d65d4170d8f3be1b953d4fad4d4cdee791f724b
AhmadRazaJamal <ahmad.raza@me.com>
2022-03-07 22:47:36 -0800

    takes in user input

--- RequestSchedular.java
+++ RequestSchedular.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class RequestSchedular {
     public static void main(String[] args){
-        int numSlaves = 2; 
-        
-        BoundedBuffer<int[]> sharedBuffer = new BoundedBuffer<>(10);
 
-        MasterThread<int[]> master = new MasterThread<int[]>(sharedBuffer, 5000, 3000);
+        Scanner sc=new Scanner(System.in); 
+        
+        System.out.println("Enter the number of slave threads to employ:"); 
+        int numSlaves = sc.nextInt(); 
+
+        System.out.println("Enter the maxiumum duration of sleep before next request(in milliseconds):"); 
+        int sleepDur = sc.nextInt(); 
+
+        System.out.println("Enter the maxiumum duration of a request(in milliseconds):"); 
+        int maxDur = sc.nextInt(); 
+        
+        BoundedBuffer<int[]> sharedBuffer = new BoundedBuffer<>(2);
+
+        MasterThread<int[]> master = new MasterThread<int[]>(sharedBuffer, sleepDur, maxDur);
 
         for(int i = 0; i < numSlaves; i++){
             SlaveThread<int[]> slave = new SlaveThread<int[]>(sharedBuffer, i + 1);
